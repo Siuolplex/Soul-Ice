45d88b4b5ebd5217b05e5644df113e6ec0060a46
Siuol <43890828+Siuolthepic@users.noreply.github.com>
2022-01-13 03:53:57 -0600

    Commiting code in half an a press.
    I held a guys I swear.
    
    Signed-off-by: Siuol <43890828+Siuolthepic@users.noreply.github.com>

--- common/src/main/java/io/siuolplex/soul_ice/mixin/EntityMixin.java
+++ common/src/main/java/io/siuolplex/soul_ice/mixin/EntityMixin.java
@@ -9,7 +9,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(Entity.class)
+@Mixin(value = Entity.class, priority = 999)
 public class EntityMixin {
     @Inject(method = "getVelocityAffectingPos()Lnet/minecraft/util/math/BlockPos;", at = @At("RETURN"), cancellable = true)
     private void injected(CallbackInfoReturnable<BlockPos> cir) {
